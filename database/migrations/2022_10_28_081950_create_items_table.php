10a5eceff70fab3ceb4034816ddc06cc6d7b735a
hcanas <harveyanas@gmail.com>
2022-11-17 14:14:15 +0800

    specify foreign table name

--- database/migrations/2022_10_28_081950_create_items_table.php
+++ database/migrations/2022_10_28_081950_create_items_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('items', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->foreignIdFor(\App\Models\ItemCategory::class, 'category_id')->constrained();
+            $table->foreignIdFor(\App\Models\ItemCategory::class, 'category_id')->constrained('item_categories');
             $table->string('commodity_type');
             $table->string('details');
         });
